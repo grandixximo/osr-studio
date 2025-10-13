469c23772053d62005d953e152ba58a21c308537
Luca Toniolo <luca@aitalmac.com>
2025-10-13 14:43:30 +0800

    Bump version to 10.7.1 and update changelog

--- src/OsrStudio.Console/Properties/AssemblyInfo.cs
+++ src/OsrStudio.Console/Properties/AssemblyInfo.cs
@@ -6,4 +6,4 @@ using System.Reflection;
 [assembly: AssemblyProduct("OSR Studio Console")]
 [assembly: AssemblyCopyright("(c) 2018 Mathew Sachin, 2024 Mr. Chip, 2025 grandixximo")]
 [assembly: AssemblyTrademark("OSR Studio Console")]
-[assembly: AssemblyVersion("10.7.0")]
+[assembly: AssemblyVersion("10.7.1")]
--- src/OsrStudio/Properties/AssemblyInfo.cs
+++ src/OsrStudio/Properties/AssemblyInfo.cs
@@ -6,4 +6,4 @@ using System.Reflection;
 [assembly: AssemblyProduct("OSR Studio")]
 [assembly: AssemblyCopyright("© 2018 Mathew Sachin, © 2024 Mr. Chip, © 2025 grandixximo")]
 [assembly: AssemblyTrademark("OSR Studio")]
-[assembly: AssemblyVersion("10.7.0")]
+[assembly: AssemblyVersion("10.7.1")]
